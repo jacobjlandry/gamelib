0fbeeea7914a99c615da413478b656a43e54bc21
Jacob Landry <jacoblandry@Jacobs-Air.hsd1.ca.comcast.net>
2016-11-26 15:46:43 -0800

    making repeated environment variables unique for this project since it will share a server

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('GAMELIB_DB_HOST', 'localhost'),
+            'port' => env('GAMELIB_DB_PORT', '3306'),
+            'database' => env('GAMELIB_DB_DATABASE', 'forge'),
+            'username' => env('GAMELIB_DB_USERNAME', 'forge'),
+            'password' => env('GAMELIB_DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
