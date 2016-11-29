c6ba461c10fa0cad0422b6a4b816876cc943ab55
Jacob Landry <jacoblandry@Jacobs-Air.hsd1.ca.comcast.net>
2016-11-28 20:50:11 -0800

    fixing various bugs in the UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,16 +8,16 @@
                 <div class="panel-heading">Welcome {{ Auth::user()->name }}</div>
                 <div class="panel-body graph-panel">
                     <div id="platformChart"></div>
-                    <div id="gamesCount"><div class="number"><a href="user/games">{{ $games->count() }}</a></div> <div class="descriptor">Total Games</div></div>
-                    <div id="platformCount"><div class="number"><a href="user/platforms">{{ $platforms->count() }}</a></div> <div class="descriptor">Platforms</div></div>
+                    <div id="gamesCount" class="user-stat"><div class="number"><a href="user/games">{{ $games->count() }}</a></div> <div class="descriptor">Total Games</div></div>
+                    <div id="platformCount" class="user-stat"><div class="number"><a href="user/platforms">{{ $platforms->count() }}</a></div> <div class="descriptor">Platforms</div></div>
                 </div>
             </div>
             <div class="panel panel-success">
                 <div class="panel-heading">My Games</div>
                 <div class="panel-body">
-                    <div id="uniqueGames"><div class="number">{{ $games->unique('game_id')->count()  }}</div><div class="descriptor">Unique Games</div></div>
-                    <div id="gamesPlayed"><div class="number"><a href="/user/games?played=1">{{ $games->where('played', 1)->count() }}</a>/<a href="/user/games?played=0">{{ $games->where('played', 0)->count() }}</a></div><div class="descriptor">Played/Unplayed</div></div>
-                    <div id="gamesBeat"><div class="number"><a href="/user/games?beat=1">{{ $games->where('beat', 1)->count() }}</a>/<a href="/user/games/?beat=0">{{ $games->where('beat', 0)->count() }}</a></div> <div class="descriptor">Beat/Unbeat</div></div>
+                    <div id="uniqueGames" class="user-stat"><div class="number">{{ $games->unique('game_id')->count()  }}</div><div class="descriptor">Unique Games</div></div>
+                    <div id="gamesPlayed" class="user-stat"><div class="number"><a href="/user/games?played=1">{{ $games->where('played', 1)->count() }}</a>/<a href="/user/games?played=0">{{ $games->where('played', 0)->count() }}</a></div><div class="descriptor">Played/Unplayed</div></div>
+                    <div id="gamesBeat" class="user-stat"><div class="number"><a href="/user/games?beat=1">{{ $games->where('beat', 1)->count() }}</a>/<a href="/user/games/?beat=0">{{ $games->where('beat', 0)->count() }}</a></div> <div class="descriptor">Beat/Unbeat</div></div>
                 </div>
             </div>
             <div class="panel panel-warning">
@@ -26,29 +26,31 @@
                     <div id="gamesRated" class="user-stat"><div class="number"><a href="/user/games?rated=1">{{ $games->filter(function($value, $key) {
                         return $value->rating > 0;
                     })->count() }}</a>/<a href="/user/games?rated=0">{{ $games->where('rating', 0)->count() }}</a></div> <div class="descriptor">Rated/Unrated</div></div>
-                    <div id="avgRating" class="user-stat"><div class="number">{{ $games->filter(function($value, $key) {
+                    <div id="avgRating" class="user-stat"><div class="number">{{ round($games->filter(function($value, $key) {
                         return $value->rating > 0;
-                    })->average('rating') }}</div><div class="descriptor">Average Rating</div></div>
+                    })->average('rating'), 1) }}</div><div class="descriptor">Average Rating</div></div>
                     <div id="favoriteGame" class="user-stat">
                         <div class="detail">
-                            <div class="list">
-                                <div class="item">
-                                    <a href="/basic/games/{{ $games->where('rating', $games->max('rating'))->first()->game()->bomb_id }}">
-                                        <div class="image">
-                                            <div class="img">
-                                                @if($games->where('rating', $games->max('rating'))->first()->game()->image)
-                                                    <img src="{{ $games->where('rating', $games->max('rating'))->first()->game()->image }}" width="125" />
-                                                @else
-                                                    <i class="fa fa-chain-broken fa-4x"></i>
-                                                @endif
+                            @if($games->where('rating', $games->max('rating'))->count())
+                                <div class="list">
+                                    <div class="item">
+                                        <a href="/basic/games/{{ $games->where('rating', $games->max('rating'))->first()->game()->bomb_id }}">
+                                            <div class="image">
+                                                <div class="img">
+                                                    @if($games->where('rating', $games->max('rating'))->first()->game()->image)
+                                                        <img src="{{ $games->where('rating', $games->max('rating'))->first()->game()->image }}" width="125" />
+                                                    @else
+                                                        <i class="fa fa-chain-broken fa-4x"></i>
+                                                    @endif
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="title">
-                                            {{ $games->where('rating', $games->max('rating'))->first()->game()->name }}
-                                        </div>
-                                    </a>
+                                            <div class="title">
+                                                {{ $games->where('rating', $games->max('rating'))->first()->game()->name }}
+                                            </div>
+                                        </a>
+                                    </div>
                                 </div>
-                            </div>
+                            @endif
                         </div>
                         <div class="descriptor">
                             Favorite Game
@@ -56,24 +58,26 @@
                     </div>
                     <div id="leastFavoriteGame" class="user-stat">
                         <div class="detail">
-                            <div class="list">
-                                <div class="item">
-                                    <a href="/basic/games/{{ $games->where('rating', $games->min('rating'))->first()->game()->bomb_id }}">
-                                        <div class="image">
-                                            <div class="img">
-                                                @if($games->where('rating', $games->min('rating'))->first()->game()->image)
-                                                    <img src="{{ $games->where('rating', $games->min('rating'))->first()->game()->image }}" width="125" />
-                                                @else
-                                                    <i class="fa fa-chain-broken fa-4x"></i>
-                                                @endif
+                            @if($games->where('rating', $games->min('rating'))->count())
+                                <div class="list">
+                                    <div class="item">
+                                        <a href="/basic/games/{{ $games->where('rating', $games->min('rating'))->first()->game()->bomb_id }}">
+                                            <div class="image">
+                                                <div class="img">
+                                                    @if($games->where('rating', $games->min('rating'))->first()->game()->image)
+                                                        <img src="{{ $games->where('rating', $games->min('rating'))->first()->game()->image }}" width="125" />
+                                                    @else
+                                                        <i class="fa fa-chain-broken fa-4x"></i>
+                                                    @endif
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="title">
-                                            {{ $games->where('rating', $games->min('rating'))->first()->game()->name }}
-                                        </div>
-                                    </a>
+                                            <div class="title">
+                                                {{ $games->where('rating', $games->min('rating'))->first()->game()->name }}
+                                            </div>
+                                        </a>
+                                    </div>
                                 </div>
-                            </div>
+                            @endif
                         </div>
                         <div class="descriptor">
                             Least Favorite Game
